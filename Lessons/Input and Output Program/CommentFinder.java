a6fc3f1323dd1690c43c7d26a3267b53d6069497
Logan <l.campbell1995@hotmail.com>
2020-08-21 00:19:15 -0500

    CommentFinder is completed! Reinforcing & Error checking is the last step

--- Lessons/Input and Output Program/CommentFinder.java
+++ Lessons/Input and Output Program/CommentFinder.java
@@ -10,11 +10,10 @@ public class CommentFinder
         currentDirectory = System.getProperty("user.dir");
         File directory = new File(System.getProperty("user.dir"));
         System.out.println("-------------------------------------");
-        String files [] = directory.list();
+        String[] files;
+        files = directory.list();
         for(String file : files)
-        {
             System.out.println(file);
-        }
         System.out.println("-------------------------------------");
         return currentDirectory;
     }
@@ -26,8 +25,8 @@ public class CommentFinder
         System.out.println("-------------------------------------");
         File directory = new File(System.getProperty("user.dir"));
         String currentDirectory = System.getProperty("user.dir");
-        System.out.println("[CURRENT DIRECTORY] : " + currentDirectory);
-        String files[] = directory.list();
+        String[] files;
+        files = directory.list();
         for(String file : files)
         {
             System.out.println(file);
@@ -38,8 +37,9 @@ public class CommentFinder
         String folderName = userInput.nextLine();
         currentDirectory = changeDirectory(folderName, currentDirectory);
         System.out.println("CURRENT : " + currentDirectory);
-        boolean pathstring = false;
-        while (pathstring)
+        String readFilePath;
+        boolean pathString = false;
+        while (!pathString)
         {
             System.out.print("Is the path directed to the folder containing file? ");
             String choice = userInput.nextLine();
@@ -48,20 +48,55 @@ public class CommentFinder
             {
                 if (choice.equals("YES"))
                 {
-                    pathstring = true;
+                    pathString = true;
+                    break;
                 }
                 else
                     {
-                        System.out.println("Add the following file(s) to the path : ");
+                        System.out.print("Add the following folder to the path : ");
                         folderName = userInput.nextLine();
-                        changeDirectory(folderName, currentDirectory);
+                        currentDirectory = changeDirectory(folderName, currentDirectory);
+                        choice = "";
                     }
             }
         }
-
+        readFilePath = currentDirectory;
         System.out.print("Enter the file name & file type ('filename.filetype') : ");
         String fileType = userInput.nextLine();
         System.out.println("[FILE TYPE :: "+ fileType + "]");
+        readFilePath += "\\" + fileType;
+        System.out.println(readFilePath);
+        try
+        {
+            Scanner read = new Scanner(new File(readFilePath));
+            FileWriter comments = new FileWriter("comments.txt");
+            FileWriter programming = new FileWriter("programming.txt");
+
+            while (read.hasNext())
+            {
+                String words = read.nextLine();
+                if (words.contains("//"))
+                {
+                    comments.write(words);
+                    comments.write("\n");
+                }
+                else
+                {
+                    programming.write(words);
+                    programming.write("\n");
+                }
+            }
+            System.out.println("-------------------------------------------------");
+            System.out.println("comments.txt and programming.txt has been created");
+            System.out.println("-------------------------------------------------");
+            programming.close();
+            comments.close();
+            read.close();
+        }
+        catch (IOException error)
+        {
+            System.out.println("ERROR IN IO");
+        }
 
     }
 }
