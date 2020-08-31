0afe39fd202787a3d51b01a2187957a7ae58403f
lc002 <72703_Csce>
2020-08-31 11:22:35 -0500

    Java Graphical User Interface :: Layouts

---
+++ Lessons/Graphical User Interface Program/Layouts.java
@@ -0,0 +1,16 @@
+public class Layouts
+{
+    /*
+        JAVA uses layout managers to have different Graphic User Interfaces.
+        The four most used layout managers are BorderLayout/BoxLayout/FlowLayout/GridLayout.
+
+        [BorderLayout]: divides its window into 5 parts that have fixed positions relative to each other. Each part can hold a component, and the size of the part controls the component's size.
+            - BorderLayout region can only contain one component.
+            - If one is left out empty then it will not show up in the window.
+            - Even thought each region on holds one component; We can make a panel hold other components and use different layouts
+
+        [BoxLayout]: allows each component to keep its own size. Stacks components in the order we add them and we can choose to stack them vertically or horizontally.
+            - Components are the size of what is contained.
+            -
+     */
+}
